cc41d67ac04852187becda3b7840d5bad8d526c7
prasmus <rasmus.prees@tptlive.ee>
2016-12-05 15:23:32 +0200

    Update exercise69.java

--- src/exercise69.java
+++ src/exercise69.java
@@ -1,17 +1,18 @@
-/**
- * Created by Praktika on 29.11.2016.
- */
 public class exercise69 {
     public static void main(String[] args) {
-        double x = 10.5;
-        int n = 3;
-        System.out.print("astendamise vastus on: " + power(x, n));
+        printTable(7);
     }
-    public static double power(double x, int n) {
-        if(n == 0) {
-            return 1;
-        } else {
-            return x * power(x, n - 1);
+        public static void printTable(int rows) {
+            for (int i = 1; i <= rows; i = i + 1) {
+                printRow(i, rows);
+            }
         }
+    public static void printRow(int n, int cols) {
+        int i = 1;
+        while (i <= cols) {
+            System.out.printf("%4d", n * i);
+            i++; //sama mis i = i + 1
+        }
+        System.out.println();
     }
 }
